db96812dd158e4f79914bd55737a9e044d32dc9f
Nilesh88033 <77041855+Nilesh88033@users.noreply.github.com>
2021-06-17 18:00:40 +0530

    Add files via upload

---
+++ Practical- Q5 -Code-Fibonacci.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int);
+void fibonaciiSeries(int);
+
+int  fibo(int num ){
+    if (num==1){
+        return 1;
+    }
+    else if(num==0){
+        return 0;
+    }
+    else{
+        return fibo(num-1) + fibo(num-2);
+    }
+}
+void fibonaciiSeries(int n){
+    for (int i = 0; i < n; i++){
+        cout<<fibo(i)<<" ";
+    }
+}
+
+int main(){   
+
+    cout<<"Fibonacii Series"<<endl;
+    fibonaciiSeries(10);
+
+    return 0;
+}
+
